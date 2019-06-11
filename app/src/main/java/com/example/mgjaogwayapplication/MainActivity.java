004bf4374e09407b18a4edff7500c952d624fd6d
Alec Alan Jackson <alecalanjackson@gmail.com>
2019-06-11 17:35:18 -0400

    aaaa

--- app/src/main/java/com/example/mgjaogwayapplication/MainActivity.java
+++ app/src/main/java/com/example/mgjaogwayapplication/MainActivity.java
@@ -10,10 +10,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+/// aaaaaaa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+          super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
